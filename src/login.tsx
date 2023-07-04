6f4a7f3223e1cfde0b7db4bae73e873ee4b50dc6
Nikodem Zareba <nikodem.zareba00@gmail.com>
2023-07-04 20:39:17 +0100

    Fixes

--- src/login.tsx
+++ src/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './login.css';
+import './form.css';
 
 const LoginScreen: React.FC = () => {
   return (
